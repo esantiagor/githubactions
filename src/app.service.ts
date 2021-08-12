1693c648fb457e30c9292b741d0586101f1f85e2
esantiagor <donatis91@gmail.com>
2021-08-12 17:32:50 -0500

    change message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello SE ENTRENA!';
+    return 'Hello SE ENTRENA 2!!';
   }
 }
